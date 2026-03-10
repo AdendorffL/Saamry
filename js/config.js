23baf1f61ee260f03d8cf1f25f61305c112bbaaf
Lourens Adendorff <lou.adendorff@gmail.com>
2026-03-10 15:44:42 +0200

    core: implement authorization and update index and config

---
+++ js/config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  SUPABASE_URL: 'https://ycrjoadtjtggqtjoidpg.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljcmpvYWR0anRnZ3F0am9pZHBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNDExMzYsImV4cCI6MjA4ODcxNzEzNn0.WkED20UrBkx2wqunh3v0HgtUr-gFrRT-fYzF6rLTxqU',
+
+  // ===== User Config =====
+  USERS: [
+    { name: 'Annelize', email: 'ottoannelize05@gmail.com', color: '#4f8ef7' },
+    { name: 'Tenisha',   email: 'tenisha.swanepoel@gmail.com', color: '#f7704f' },
+    { name: 'Kelsy', email: 'ferreirakelsy11@gmail.com', color: '#4fd98e' },
+  ] 
+};
